b77279a8738b5187ce757932b5eeef3dd5e68d0d
Informatic PC <informaticpc07@gmail.com>
2024-07-16 10:38:20 +0100

    Explanation of entries and values Obj method

--- 09-Data-Structures-Operators/starter/LoopingObj.mjs
+++ 09-Data-Structures-Operators/starter/LoopingObj.mjs
@@ -2,6 +2,7 @@
 
 import { books } from './module.mjs';
 
+// https://codingheroes.io/assignments/looping-objects-object-keys-values-and-entries.html
 // 11.1
 //Below is the entries variable that stores an empty array. Use the for-of loop together with the Object.keys() method to loop over the thirdParty.goodreads property (array) of the first book object from the books array. For each key, push a new array that contains that key to the entries array.
 
@@ -19,9 +20,20 @@ import { books } from './module.mjs';
 const entries = [];
 
 for (const keys of Object.keys(books[0].thirdParty.goodreads)) {
+  console.log('what is "keys"?: ' + keys);
   entries.push([keys]); // understand why putting 'key' as an array '[]' creates subarrays...x`
 }
+console.log(`Object.keys(array/obj)`);
 console.log(entries);
+
+for (const arrEntries of entries.entries()) {
+  console.log(
+    `SHOW ME NOW HOW .entries() METHOD LOOKS LIEK IN entries[] array \n ${arrEntries}`
+  );
+}
+for (let index = 0; index < entries.length; index++) {
+  const element = entries[index];
+}
 
 // 11.2
 
@@ -36,6 +48,7 @@ console.log('#######################');
 for (const [property, value] of Object.values(
   books[0].thirdParty.goodreads
 ).entries()) {
+  console.log(`what is property it self?:  ${property}`);
   console.log(`proper= ${entries[property]} value= ${value}`);
   entries[property].push(value);
   console.log(entries[property]);
@@ -46,30 +59,41 @@ console.log('TEST#######################');
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
 // crate const just to store the value of porperty
 
-function explanatoryCode() {
-  for (const property of Object.values(books[0].thirdParty.goodreads)) {
-    console.log('property_values: ' + property);
-    // 'values' return an array but here we are iterating that array right away
-    // thats why we see the values instead the array.
-    /*
-    // for (const [prop, val] of Object.entries(property)) {
-    //   console.log('Entries:');
-    //   console.log('propertty: ' + prop);
-    //   console.log('value: ' + val);
-    // }*/
+function explanatoryCode_1() {
+  const arryToReturn = [];
+  console.log('----------EXPLANATORY CODE 1----------');
+  for (const [index, prop, val] of Object.entries(
+    books[0].thirdParty.goodreads
+  ).entries()) {
+    console.log('Entries:');
+    console.log('index: ');
+    console.log(index);
+    console.log('prop: ');
+    console.log([prop]);
+    console.log('value: ');
+    console.log(val);
 
-    for (const [prop, val] of Object.entries(property)) {
-      console.log('Entries:');
-      console.log('prop: ' + entries[prop]);
-      console.log('value: ' + val);
-    }
+    arryToReturn.push([prop]);
+    console.log(`ARRAY TO RETURN: ${arryToReturn}`);
   }
+  console.log('loop FINISHED ------------');
 }
-explanatoryCode();
-const value = Object.values(books[0].thirdParty.goodreads);
-console.log(`return array of values???: ${value}`);
+function explanatoryCode_2() {
+  const arryToReturn = [];
+  console.log('----------EXPLANATORY CODE 2----------');
+  for (const [prop, value] of Object.entries(books[0].thirdParty.goodreads)) {
+    console.log('prop: \n' + prop + '\n value: \n' + value);
+
+    // console.log('value: ' + val);
+    // arryToReturn[prop].push(val);
+    // console.log(`ARRAY TO RETURN: ${arryToReturn}`);
+  }
+  console.log('loop FINISHED ------------');
+}
+explanatoryCode_1();
+explanatoryCode_2();
 /* OUTPUT:
-TEST#######################
+TEST#######################T
 property_values: 4.41,545
 Entries:
 prop: rating,4.41,545
@@ -83,6 +107,18 @@ property_values: 976
 property_values: 13
 return array of values???: 4.41,545,1733,63,976,13
 */
+/**
+ * UNDERSTOOD: SO 'entries[property]'  IS ALREADY AN ARRAY CRATED IN THE 1ST EXERCISE*/
+/**
+ * 'entries.push([keys])' ADD THE KEYS/PROPERTYNAME AS NEW ARRAYS []
+ * TO THE 'entries[]'ARRAY
+ */
+/**
+ * YOU PUSH THEM LIKE NEW EACH ARRAY '[keys]' BECAUSE ON THIS WAY YOU CAN USE '[].PUSH()'
+ * METHOD TO ADD NEW ELEMENTS
+ * OTHERWISE IT WILL TREAT THE VALUES LIKE STRINGS OR NUMBER AND
+ * YOU WON'T BE ABLE TO USE '.PUSH()' METHOD
+ */
 
 // 11.3
 // Use the Object.entries() method on the thirdParty.goodreads property of the first book from the books array. Assign the returned value to the variable called entries2.
